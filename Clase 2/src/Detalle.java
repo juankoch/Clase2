9db301eca195a8581472aacfa56cb60e3a4d2494
juankoch <juan.koch@davinci.edu.ar>
2024-08-27 22:52:47 -0300

    Completo

--- Clase 2/src/Detalle.java
+++ Clase 2/src/Detalle.java
@@ -1,4 +1,62 @@
 
 public class Detalle {
 
+	private String descripcion;
+	private int cantCuotas;
+	private double ValorTotal;
+	private double valorXcuotas;
+	
+	public Detalle(String descripcion, int cantCuotas, double valorTotal, double valorXcuotas) {
+		super();
+		this.descripcion = descripcion;
+		this.cantCuotas = cantCuotas;
+		ValorTotal = valorTotal;
+		this.valorXcuotas = valorXcuotas;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getCantCuotas() {
+		return cantCuotas;
+	}
+
+	public void setCantCuotas(int cantCuotas) {
+		this.cantCuotas = cantCuotas;
+	}
+
+	public double getValorTotal() {
+		return ValorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		ValorTotal = valorTotal;
+	}
+
+	public double getValorXcuotas() {
+		return valorXcuotas;
+	}
+
+	public void setValorXcuotas(double valorXcuotas) {
+		this.valorXcuotas = valorXcuotas;
+	}
+
+	@Override
+	public String toString() {
+		return "Detalle [descripcion=" + descripcion + ", cantCuotas=" + cantCuotas + ", ValorTotal=" + ValorTotal
+				+ ", valorXcuotas=" + valorXcuotas + ", getDescripcion()=" + getDescripcion() + ", getCantCuotas()="
+				+ getCantCuotas() + ", getValorTotal()=" + getValorTotal() + ", getValorXcuotas()=" + getValorXcuotas()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+	
+	
+	
 }
--- Clase 2/src/Titular.java
+++ Clase 2/src/Titular.java
@@ -1,4 +1,27 @@
 
 public class Titular {
-
+	private String nombre;
+	private double saldo;
+	public Titular(String nombre, double saldo) {
+		super();
+		this.nombre = nombre;
+		this.saldo = saldo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	@Override
+	public String toString() {
+		return "Detalle [nombre=" + nombre + ", saldo=" + saldo + "]";
+	}
+	
 }
